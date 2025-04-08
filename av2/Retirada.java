6a083ea9e9990d0acad19314e4e937bbfa1d621c
Lucas Neves <lfneves9@gmail.com>
2025-04-07 22:20:13 -0300

    Delete av2/Retirada.java

--- av2/Retirada.java
+++
@@ -1,9 +0,0 @@
-package av2;
-
-public class Retirada implements Despacho{ //classe implementa a interface Despacho para a retirada de produtos do estoque 
-    @Override
-    public void executar(Produto p, int quantidade) {
-        p.removeQuantidade(quantidade);
-        System.out.println("Retirada de " + quantidade + " itens");
-    }
-}
